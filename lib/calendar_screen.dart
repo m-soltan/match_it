e63470f1255e918f3d73c0f78030188013b592bc
Mikołaj Sołtan <mikolaj.soltan0@gmail.com>
2022-01-11 16:33:13 +0100

    add empty screens for list screen to link to

---
+++ lib/calendar_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CalendarScreen extends StatelessWidget {
+  @override
+  Widget build(_) => Scaffold();
+}
---
+++ lib/definition_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class DefinitionScreen extends StatelessWidget {
+  @override
+  Widget build(_) => Scaffold();
+}
